8f5c3a2bb2336f8b4eae673c603c631dfecdd4c0
Artur Smęt <artur.smet@gmail.com>
2012-09-05 12:12:32 +0200

    dry run parameter

--- detect_uwsgi.py
+++ detect_uwsgi.py
@@ -6,8 +6,9 @@ import supervisord
 import socket
 import logging
 
-def detect_apps(srv_dir):
+def detect_apps(srv_dir, dry_run=False):
     dirs = os.listdir(srv_dir)
+    os.chdir(srv_dir)
     for d in dirs:
         # Check if domain is active (DNS check)
         try:
@@ -19,12 +20,15 @@ def detect_apps(srv_dir):
         if "uwsgi.ini" in os.listdir(d):
             # We've found uWSGI app!
             # Now create nginx vhost based on dir name
-            nginx.add_uwsgi_vhost(
-                    domain_name=d,
-                    document_root=d
-                    )
-            # Enable supervisord for uWSGI process
-            supervisord.new_app(app_name=d, app_dir=d)
-            nginx.enable_vhost(d)
+            if dry_run:
+                logging.info("uWSGI app found in %s" % d)
+            else:
+                nginx.add_uwsgi_vhost(
+                        domain_name=d,
+                        document_root=d
+                        )
+                # Enable supervisord for uWSGI process
+                supervisord.new_app(app_name=d, app_dir=d)
+                nginx.enable_vhost(d)
 
 
